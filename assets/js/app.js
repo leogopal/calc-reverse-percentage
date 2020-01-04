a128e6656a5886a6a6e12f53d8e21109161c168f
Leo Gopal <leo@leogopal.com>
2020-01-04 08:18:48 +0200

    First working version of Calculator

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+function calcReversePercent() {
+    var totalwithpercent = document.getElementById("totalwithpercent").value;
+    var percentage = document.getElementById("percentage").value;
+
+    var totalBeforePercent =  totalwithpercent / ( (percentage / 100) + 1) ;
+    var percentageAmount = totalwithpercent - totalBeforePercent;
+
+    document.getElementById("totalBeforePercent").innerHTML = totalBeforePercent;
+    document.getElementById("percentValue").innerHTML = percentageAmount;
+    document.getElementById("finalTotal").innerHTML = totalwithpercent;
+}
+
+
