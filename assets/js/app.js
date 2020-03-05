9c126711a9525fa210e7f1bde9d4f734a23ab722
Leo Gopal <leo@leogopal.com>
2020-03-05 12:21:51 +0200

    working calc

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,11 @@
 function calcReversePercent() {
-
     var totalwithpercent = document.getElementById("totalwithpercent").value;
     var percentage = document.getElementById("percentage").value;
 
-    var totalBeforePercent =  calculateReversePercentage(totalwithpercent, percentage) ;
+    var totalBeforePercent =  totalwithpercent / ( (percentage / 100) + 1) ;
     var percentageAmount = totalwithpercent - totalBeforePercent;
 
     document.getElementById("totalBeforePercent").innerHTML = totalBeforePercent;
     document.getElementById("percentValue").innerHTML = percentageAmount;
     document.getElementById("finalTotal").innerHTML = totalwithpercent;
-
-}
-
-function calculateReversePercentage(gross, percent) {
-    return gross / ( (percent / 100) + 1) ;
-}
-
-function calculatePercentageVariance() {
-
 }
